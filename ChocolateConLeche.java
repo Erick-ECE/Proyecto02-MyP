a4f77e1f26060ada6617e12eae9de8a65fa9be81
owner <ericken15@ciencias.unam.mx>
2020-06-04 17:51:03 -0500

    ChocolateConLeche

--- ChocolateConLeche.java
+++ ChocolateConLeche.java
@@ -1,3 +1,14 @@
 public class ChocolateConLeche extends Dulce {
+
+    public ChocolateConLeche() {
+        tipo= "Chocolate Con Leche";
+    }
+
+    @Override
+    public String getTipo() {
+        // TODO Auto-generated method stub
+        return super.getTipo();
+    }
+    
     
 }
