d16e2acc403ff1397ae35fa0eabbdd76f4f91b3d
desalessr <desales@ciencias.unam.mx>
2020-06-05 00:33:36 -0500

    procesamiento de los pedidos (y no en funcional >:C

--- JefeTecnico.java
+++ JefeTecnico.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
 * JefeTecnico Clase que modela el jefe técnico del sistema 
@@ -13,17 +14,50 @@ import java.util.ArrayList;
 
 public class JefeTecnico implements Observer {
     
+    //Objeto random
+    Random rnd; 
     //Repartidores que podrán realizar las entregas
-    private ArrayList<Repartidor> repartidores;
+    private ArrayList<Repartidor> repartidores; // INICIA CON 7
     //Proxys de las sucursales para revisar sus inventarios
     private ArrayList<SucursalProxy> sucursalProxys; 
     //Referencia a la máquina wonka que utiliza
     Wonka wonka;
 
+
+    //suponiendo que lo que se tiene que construir ya lo está
+    
     /**
-     * Constructor 
-     * @param repartidores Los
+     * Método que recibe el pedido pedido que se va a procesar
+     * @param pedido Cadena con el pedido
      */
+    public void recibirPedido(ArrayList<String> pedido, int id) {
+        
+        ArrayList<Lote> pedidoTotal = new ArrayList<>();
 
+        //Obtenemos el proxy
+        SucursalProxy proxy = null;
+        for(SucursalProxy sp : sucursalProxys){
+            if (sp.getId() ==id)
+                proxy = sp;
+        }
 
-}
+        for(String p : pedido ){
+            if (proxy.consultarInventario(p) == 0) {
+                //No hay, entonces preparas
+                if (wonka.validarPedido(p)) {
+                    pedidoTotal.add(wonka.prepararDulce(p));
+                } else {
+                    System.out.println("Tienes, pa que quieres \n");
+                }
+            }
+        }
+    }
+
+    /**
+     * Método de la interfaz
+     * @param o 
+     */
+    public void update(ArrayList<String> pedido, int id) {
+        recibirPedido(pedido, id);
+    }
+} 
--- SucursalProxy.java
+++ SucursalProxy.java
@@ -6,21 +6,28 @@
 
 public class SucursalProxy implements InterfazConsulta{
     //Sucursal con la que realiza la conexión 
-    Sucursal suc; 
+    Sucursal sucursal; 
 
     /**
      * Constructor
      * @param s La Sucursal
      */
-    public SucursalProxy(Sucursal s) {suc = s }
+    public SucursalProxy{Sucursal sucursal) {this.sucursal = sucursal; }
+
+    /**
+     * Método que regresa el id de la sucursal a la que hace referencia el
+     * el peoxy
+     * @return id 
+     */
+    public int getId(){ return sucursal.getId(); }
 
     /**
      * Método de la interfaz 
      * @param tipoDeDulce La descripcion del tipo de dulce que 
      * se desea buscar
      */
-    public int consultarInventario(String tipoDeDulce) {
-        s.consultarInventario(tipoDeDulce);
+    public int consultarInventario(final String tipoDeDulce) {
+        sucursal.consultarInventario(tipoDeDulce);
     }
 
 
