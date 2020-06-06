e4cc26d8c556d1f38c059be460b001a30f755832
desalessr <desales@ciencias.unam.mx>
2020-06-05 21:49:00 -0500

    Clase receta uwu

---
+++ Receta.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+/**
+ * Receta. Clase que modela la receta de un dulce. 
+ * @author Daniel Villegas & Erick Castro & Ricardo Desales 
+ */
+public class Receta {
+
+    //Nombre de la receta
+    String nombre;
+    //Lista con los ingredientes de la receta
+    ArrayList<Ingrediente> ingredientes;
+
+    /**
+     * Constructor de la receta
+     * @param nombre Nombre del dulce a preparar
+     * @param ingredientes Ingredientes del dulce
+     */
+    public Receta(String nombre, ArrayList<Ingrediente> ingredientes) {
+        this.nombre = nombre;
+        this.ingredientes = ingredientes; 
+    }
+
+    /**
+     * Método que regresa los ingredientes de la receta. _
+     */
+    public ArrayList<Ingrediente> getReceta(){
+        return ingredientes;
+    }
+}
